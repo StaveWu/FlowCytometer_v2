def2bf1fb1f8bfca6ece20ef6d3c600196cc22e4
腾达 吴 <wtengda0816@outlook.com>
2019-06-17 20:18:45 +0800

    slightly change channel display

--- src/main/java/application/channel/ChannelApp.java
+++ src/main/java/application/channel/ChannelApp.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 public class ChannelApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-//        FCMRunTimeConfig.getInstance().setRootDir("E:\\04文档\\陈宇欣\\流式细胞仪\\软件项目树测试");
+        FCMRunTimeConfig.getInstance().setRootDir("E:\\04文档\\陈宇欣\\流式细胞仪\\软件项目树测试");
         Channel channel = new Channel();
         Scene scene = new Scene(channel);
         primaryStage.setTitle("Channel");
