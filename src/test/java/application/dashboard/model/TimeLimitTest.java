0609689c58fa5403fb3bc99c3634f1edddb7342e
腾达 吴 <wtengda0816@outlook.com>
2019-06-19 11:05:24 +0800

    time limit unit test

---
+++ src/test/java/application/dashboard/model/TimeLimitTest.java
@@ -0,0 +1,29 @@
+package application.dashboard.model;
+
+import org.junit.Test;
+
+import java.sql.Time;
+
+import static org.junit.Assert.*;
+
+public class TimeLimitTest {
+
+    @Test
+    public void totalSeconds() {
+        TimeLimit timeLimit = new TimeLimit(1, 1, 1);
+        assertEquals(3661, timeLimit.totalSeconds());
+    }
+
+    @Test
+    public void formatSeconds() {
+        TimeLimit timeLimit = TimeLimit.formatSeconds(3661);
+        assertEquals(1, timeLimit.hours);
+        assertEquals(1, timeLimit.minutes);
+        assertEquals(1, timeLimit.seconds);
+    }
+
+    @Test
+    public void toStringTest() {
+        assertEquals("01:01:01", TimeLimit.formatSeconds(3661).toString());
+    }
+}
