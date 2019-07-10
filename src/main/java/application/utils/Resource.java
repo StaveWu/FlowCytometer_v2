7e5615ac2302dac9fbe4993030838e1d09541a0d
腾达 吴 <wtengda0816@outlook.com>
2019-07-10 18:37:15 +0800

    refactor resource class, more stablely

--- src/main/java/application/utils/Resource.java
+++ src/main/java/application/utils/Resource.java
@@ -5,22 +5,25 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.net.URL;
+import java.util.Objects;
 
+/**
+ * A class to load fxml, css and icons from resource folder.
+ */
 public class Resource {
 
-    private static final String BASE_URL = "../../../resources";
-
     public static URL getFXML(String name) {
-        return Resource.class.getResource(String.format("%s/fxml/%s", BASE_URL, name));
+        return Resource.class.getClassLoader().getResource(String.format("fxml/%s", name));
     }
 
     public static Node getIcon(String name) {
-        return new ImageView(new Image(Resource.class
-                .getResourceAsStream(String.format("%s/icons/%s", BASE_URL, name))));
+        return new ImageView(new Image(Objects.requireNonNull(Resource.class.getClassLoader()
+                .getResourceAsStream(String.format("icons/%s", name)))));
     }
 
     public static String getStyle(String name) {
-        return Resource.class.getResource(String.format("%s/fxml/%s", BASE_URL, name))
+        return Objects.requireNonNull(Resource.class.getClassLoader()
+                .getResource(String.format("fxml/%s", name)))
                 .toExternalForm();
     }
 
